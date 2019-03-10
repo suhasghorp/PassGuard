06b81d8ef5208172d82425a27d83e6348b969544
suhasghorp <suhasghorp@gmail.com>
2019-03-10 18:15:09 -0400

    Add project files.

---
+++ PassGuard2/PassGuard2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PassGuard2.Models;
+
+namespace PassGuard2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
